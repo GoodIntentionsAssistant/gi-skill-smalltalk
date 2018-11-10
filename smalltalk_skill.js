8bb42c811db75831bb5c3e581f901c59b77d6750
Darren Moore <darren.m@firecreekweb.com>
2018-11-10 21:18:28 +0700

    Init

---
+++ smalltalk_skill.js
@@ -0,0 +1,19 @@
+/**
+ * Small Talk Skill
+ */
+var Skill = require('../../../src/Skill/skill');
+
+module.exports = class SmalltalkSkill extends Skill {
+
+/**
+ * Constructor
+ *
+ * @param App app
+ * @access public
+ * @return void
+ */
+	constructor(app) {
+		super(app);
+	}
+
+}
